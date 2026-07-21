262a21742c73b3d7632786b080c64051626599b9
Wascar Perez <wascar.perez@yahoo.com>
2026-07-21 06:52:07 -0600

    Add configuration file for environment variables and settings

---
+++ producers/config.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALPHA_VANTAGE_API_KEY=os.environ["ALPHA_VANTAGE_API_KEY"]
+FRED_API_KEY=os.environ["FRED_API_KEY"]
+KAFKA_BOOTSTRAP_SERVERS=os.environ["KAFKA_BOOTSTRAP_SERVERS", "localhost:19092"]
+
+FX_PAIRS = [("USD", "EUR"), ("USD", "JPY")]
+FX_POLL_INTERVALS_SECONDS = 90
+
+FRED_SERIES = ["UNRATE", "FEDFUNDS", "CPIAUCSL"]
+FRED_REPLAY_INTERVAL_SECONDS = 1
+FRED_CACHE_PATH = "fred_cache.json"
